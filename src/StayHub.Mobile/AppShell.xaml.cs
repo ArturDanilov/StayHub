68a3595124ee90d3b35fd4644449e9474dd7ba18
Artur Danilov <arthurdanilov1990@gmail.com>
2026-09-04 13:53:29 +0200

    Add initial .NET MAUI project setup for StayHub Mobile app

---
+++ src/StayHub.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StayHub.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/StayHub.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StayHub.Mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
