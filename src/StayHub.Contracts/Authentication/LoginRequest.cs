350db99b04c40b9e88a0507ad089b522cc083820
Artur Danilov <arthurdanilov1990@gmail.com>
2026-09-09 17:27:01 +0200

    Add validation attributes to `LoginRequest` properties for `Username` and `Password`

--- src/StayHub.Contracts/Authentication/LoginRequest.cs
+++ src/StayHub.Contracts/Authentication/LoginRequest.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StayHub.Contracts.Authentication;
 
 public sealed record LoginRequest(
+    [property: Required, StringLength(100, MinimumLength = 1)]
     string Username,
+    [property: Required, StringLength(256, MinimumLength = 1)]
     string Password);
